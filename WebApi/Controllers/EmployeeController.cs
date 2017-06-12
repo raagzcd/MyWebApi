f5dd2387e4a96e107524d9a664e6b74b105ad804
Raghavendra Toranalu <raagzcd@gmail.com>
2017-06-11 21:22:40 -0400

    Initial commit

---
+++ WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    
+    public class EmployeeController : ApiController
+    {
+        private static List<Employee> data = new List<Employee>();
+        // GET api/values
+        public IEnumerable<Employee> Get()
+        {
+            //return new string[] { "value1", "value2" };            
+            data.Add(new Employee(){ BirthDate = DateTime.MinValue, DepartmentId =1, FirstName = "Raghu", LastName = "toranalu", Id = 1 });
+            data.Add(new Employee() { BirthDate = DateTime.MinValue, DepartmentId = 1, FirstName = "Reo", LastName = "Bernal", Id = 2 });
+            data.Add(new Employee() { BirthDate = DateTime.MinValue, DepartmentId = 1, FirstName = "Siva", LastName = "Kumar", Id = 3 });
+            data.Add(new Employee() { BirthDate = DateTime.MinValue, DepartmentId = 1, FirstName = "Siva", LastName = "Kumar", Id = 4 });
+            return data;
+        }
+
+        // GET api/values/5
+        public Employee Get(int id)
+        {
+            if (data != null)
+            {
+                return data.FirstOrDefault(s => s.Id == id );
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        // POST api/values
+        public void Post([FromBody]Employee employee)
+        {
+            try
+            {
+                var _employee = new Employee()
+                {
+                    Id = 1,
+                    FirstName = employee.FirstName,
+                    LastName = employee.LastName,
+                    BirthDate = employee.BirthDate,
+                    DepartmentId = employee.DepartmentId
+                };
+
+                if (_employee == null)
+                {
+                    throw new ArgumentNullException();
+                }
+
+                data.Add(_employee);                
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApi/Models/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using Newtonsoft.Json;
+
+namespace WebApi.Models
+{
+    public class Employee 
+    {
+        public Employee()
+        {
+            
+        }
+
+        [JsonProperty("id")]
+        [Key, Column(Order = 0)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(120)]
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(120)]
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [JsonProperty("birthDate")]
+        public DateTime BirthDate { get; set; }
+
+        
+
+        [ForeignKey("Department")]
+        [JsonProperty("departmentId")]
+        public int DepartmentId { get; set; }
+
+        
+    }
+
+    
+}
